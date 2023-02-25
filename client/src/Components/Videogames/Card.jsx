c37cf822f6ad984e17cddda6bfa01c9f809246b5
Danius98 <luisdanielarciniegasbarco@gmail.com>
2023-02-25 15:06:46 -0500

    card styles retouched

--- client/src/Components/Videogames/Card.jsx
+++ client/src/Components/Videogames/Card.jsx
@@ -14,7 +14,7 @@ export default function GameCard(props) {
             <div class="ola" ><svg class="wave" viewBox="0 0 500 150" preserveAspectRatio="none"><path class="path" d="M-115.97,-8.36 C590.57,-64.61 151.52,63.66 500.00,49.98 L500.00,0.00 L96.22,-7.38 Z"></path></svg></div>
            <div className="align">
            <img class="image" src={Imagen} alt="No imagen"/>
-              <h5>{Generos?.map((e) => e.Genero).join(' ')}</h5>
+              <h1 class="genre_name">{Generos?.map((e) => e.Genero).join(`\n`)}</h1>
            </div>
         </div>
         </Link>
@@ -30,4 +30,5 @@ export default function GameCard(props) {
         </div>
         </div>
     )
+    
 };
